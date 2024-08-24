51cb02598eee190301df33507a0eaea5e7a5b368
zaidanpasya <147239142+zprxzai@users.noreply.github.com>
2024-08-24 12:43:13 +0700

    Add files via upload

---
+++ brillian/main.js
@@ -0,0 +1,8 @@
+let name = prompt("siapa namamu");
+if (name.includes("brillian")) {
+let nama =  alert("selamat datang brillian😇");
+  window.location = "revenge.html";
+} else {
+  window.location = "login.html";
+}
+
